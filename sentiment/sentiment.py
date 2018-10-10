67b03e00085121101f23882729d22a9d363e094b
Djane Rey Mabelin <d.mabelin@arcanys.com>
2018-10-10 16:07:43 +0800

    clean up

--- sentiment/sentiment.py
+++ sentiment/sentiment.py
@@ -1,3 +1,4 @@
+import re
 
 K = 0.01
 
@@ -6,6 +7,9 @@ def build_model(data):
     model = {}
 
     for sentence, labels in data:
+        sentence = sentence.lower()
+        sentence = re.sub("[^a-z ]", "", sentence)
+        sentence = re.sub("[\n]", " ", sentence)
         for word in sentence.split(" "):
             if word:
                 if word not in model.keys():
@@ -21,6 +25,7 @@ def build_model(data):
 
 def predict(model, data):
     prediction = {}
+    data = data.lower()
     for word in data.split(" "):
         if word in model.keys():
             for field in model[word].keys():
